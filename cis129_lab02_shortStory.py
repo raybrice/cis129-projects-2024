15e1d56df5f062a94180c6ceb5490a457e0b0612
Ray Brice <64496222+raybrice@users.noreply.github.com>
2024-09-22 14:14:20 -0700

    Update cis129_lab02_shortStory.py

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -2,7 +2,7 @@
 # my First Github for the Fall 2024 session
 # prints a simple story
 
-print('Hello World!")
+print('Hello World!')
 print('Basketball is fun, we won!')
 print('We outrebounded the other team so we now get gatorade at practice on Monday!')
       
